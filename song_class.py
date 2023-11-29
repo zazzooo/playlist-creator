863dffe421622cd7e9f45b7528700dcba0cc699b
zazzooo <lorenzo.gini1998@gmail.com>
2023-11-29 12:22:20 +0100

    create better class method
    still some issue with the final code but higly improved!

--- song_class.py
+++ song_class.py
@@ -35,32 +35,85 @@ class song:
         self.boost_keys = dict_keys[self.key][1]
 
     def __str__(self):
-        return f' Song: {self.name} \n Key: {self.key} \n Compatible songs: {self.list_compatible} \n Boost songs: {self.list_boost}'
+        return f' Song: {self.name} \n Key: {self.key}'# \n Compatible songs: {self.list_compatible} \n Boost songs: {self.list_boost}'
 
     def recomended_song(self, playlist):
         list_compatible = []
         list_boost = []
         for song in playlist:
             if song.key in self.compatible_keys:
-                list_compatible.append((song.name, song.key))
+                list_compatible.append(song)
             if song.key in self.boost_keys:
-                list_boost.append((song.name, song.key))
+                list_boost.append(song)
         self.list_compatible = list_compatible
         self.list_boost = list_boost
 
     def match_compatible_song(self):
-        return random.choice(self.list_compatible)
-    
-    def match_boost_song(self):
-        return random.choice(self.list_boost)
+        try:
+            return random.choice(self.list_compatible)
+        except IndexError:
+            print('No more compatible song available')
 
-# first_song = song('name_song1', '1B')
-# final_order = []
-# final_order[0] = first_song
-# playlist = [song('NAME2', '3A'), song('name3', '1A') , song('name4', '7A'), song('name5', '9A')]
-# first_song.recomended_song(playlist)
-# final_order[-1] = final_order[-1].match_compatilble_song()
+    def match_boost_song(self):
+        try:
+            return random.choice(self.list_boost)
+        except IndexError:
+            print('No more boost song available')
+
+first_song = song('name_song1', '1B')
+final_order = []
+final_order.append(first_song)
+playlist = [song('NAME2', '3A'), song('name3', '1A') , song('name4', '7A'), song('name5', '9A'), song('name10', '8B'), song('tutturuttutu', '6B'),
+            song('piosdbioì', '4A')]
+# print(playlist)
+
+first_song.recomended_song(playlist)
+# next_song = first_song.match_compatible_song()
+# playlist.remove(next_song)
+
+previous_song = first_song
+i = 1
+while playlist != []:
+    if 3//i == 0:
+        previous_song.recomended_song(playlist)
+        next_song = previous_song.match_boost_song()
+        #print(next_song.list_boost)
+        print(next_song)
+        print('-------------')
+        for k in playlist:
+            print(k)
+        playlist.remove(next_song)
+        final_order.append(next_song)
+        previous_song = next_song
+        i += 1
+    else:
+        previous_song.recomended_song(playlist)
+        print(previous_song.list_compatible)
+        next_song = previous_song.match_compatible_song()
+        print('-------------')
+        for k in playlist:
+            print(k)
+        playlist.remove(next_song)
+        final_order.append(next_song)
+        previous_song = next_song
+        i += 1
+for i, canzone in enumerate(final_order):
+    print(f'{i} song:')
+    print(canzone)
+# final_order.append(final_or
+# der[-1].match_compatible_song())
 # print(final_order)
+# last_song = final_order[-1]
+# print(last_song)
+# playlist.remove(last_song)
+# print(playlist)
+# last_song.recomended_song(playlist)
+
+
+# while last_song.list_compatible != [] or last_song.list_boost != []:
+
+
+
 
 # if __name__ == '__manin__':
 #     first_song = song('name_song1', '1B')
